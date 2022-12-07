6eeba15c17ed80d29804b164309cb74e1caf9392
gitleonardo <br.com.leonardo@gmail.com>
2022-12-06 22:28:00 -0300

    flex-layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTabsModule} from '@angular/material/tabs';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
